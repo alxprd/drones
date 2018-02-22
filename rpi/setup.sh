1f071a02f5ccaef34355a0548a692918321fdb51
Alejandro Pereda <atk1005@gmail.com>
2018-02-23 00:41:45 +0100

    Adds Bonjour/Zeroconf to RPi.

--- rpi/setup.sh
+++ rpi/setup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+# Installs Bonjour/Zeroconf (to access the rpi as 'raspberryname.local'):
+sudo apt-get update
+sudo apt-get install libnss-mdns
+
 sudo docker build -t alxprd/rpi-gstreamer -f stream/Dockerfile_gstreamer ./stream
