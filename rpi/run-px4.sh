3bca923f2c93cc5373e8169d0784be0f8915cc5b
Alejandro Pereda <atk1005@gmail.com>
2018-02-28 21:20:35 +0100

    MOdifications in RPi autopilot.

--- rpi/run-px4.sh
+++ rpi/run-px4.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo docker run -it --name autopilot --privileged -p 5605:5605/udp --rm alxprd/rpi-px4 192.168.178.41 5605
+#sudo docker run -it --name autopilot --privileged -p 5605:5605/udp --rm alxprd/rpi-px4
+sudo docker run -it --name autopilot --privileged -p 5605:5605/udp --rm px4io/px4-dev-raspi
--- rpi/setup.sh
+++ rpi/setup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # Installs Bonjour/Zeroconf (to access the rpi as 'raspberryname.local'):
-sudo apt-get update
-sudo apt-get install libnss-mdns
+#sudo apt-get update
+#sudo apt-get install libnss-mdns
 
-sudo docker build -t alxprd/rpi-gstreamer -f stream/Dockerfile_gstreamer ./stream
+sudo docker build -t resin/rpi-raspbian ../workspace/sources/resin-rpi-raspbian/ -f ../workspace/sources/resin-rpi-raspbian/Dockerfile
+sudo docker build -t px4io/px4-dev-base ../workspace/sources/px4-containers/docker/px4-dev/ -f ../workspace/sources/px4-containers/docker/px4-dev/Dockerfile_base
+#sudo docker build -t px4io/px4-dev-armhf ../workspace/sources/px4-containers/docker/px4-dev/ -f ../workspace/sources/px4-containers/docker/px4-dev/Dockerfile_armhf
+sudo docker build -t px4io/px4-dev-raspi ../workspace/sources/px4-containers/docker/px4-dev/ -f ../workspace/sources/px4-containers/docker/px4-dev/Dockerfile_raspi
+
+#sudo docker build -t alxprd/rpi-gstreamer -f stream/Dockerfile_gstreamer ./stream
 sudo docker build -t alxprd/rpi-px4 -f autopilot/Dockerfile_px4 ./autopilot
